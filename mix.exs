358b115278c7d21191250d14ac9f9b24cd200080
Mihai Potra <mike@mpotra.com>
2021-11-03 12:28:09 +0200

    Add credo

--- mix.exs
+++ mix.exs
@@ -32,7 +32,8 @@ defmodule XdiffPlus.MixProject do
       {:saxy, "~> 1.4.0", only: [:test]},
 
       # Development dialyzer
-      {:dialyxir, "~> 1.1.0", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.1.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
     ]
   end
 end
